cc4290a54be4d859e1360d77a9502f3437e1ac97
ThibsG <thibsg@pm.me>
2021-05-16 16:59:57 +0200

    Add verbose option to command line

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 use sgx_cpu::SgxCpuInfo;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub struct Opts {
+    #[structopt(short, long)]
+    verbose: bool,
+}
 
 fn main() {
     println!("SGX CPU information\n");
+    let opts = Opts::from_args();
 
     let sgx_cpu_info = SgxCpuInfo::new();
-    println!("{}", sgx_cpu_info);
+    if opts.verbose {
+        println!("{:?}", sgx_cpu_info);
+    } else {
+        println!("{}", sgx_cpu_info);
+    }
 }
